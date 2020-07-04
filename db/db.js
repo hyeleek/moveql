bf9d11c56189cd05742b60f2da868b004c58e215
Hyelee Kim <hyeleek@andrew.cmu.edu>
2020-07-03 20:53:45 -0700

    Wrapping with MovieAPI

--- db/db.js
+++ db/db.js
@@ -1,57 +1,35 @@
-export let movies = [
-    {
-        id : 1,
-        name : "Her", 
-        score : 95.5
-    }, 
-    {
-        id : 2,
-        name : "Call me by your name", 
-        score : 98.1
-    },
-    {
-        id : 3,
-        name : "Lalaland", 
-        score : 85.2
-    },
-    {
-        id : 4,
-        name : "Starwars", 
-        score : 10
-    },
-    {
-        id : 5,
-        name : "Antman", 
-        score : 89.1
-    },
-    {
-        id : 6,
-        name : "Logan", 
-        score : 73.3
-    },
-]
+import fetch from "node-fetch";
 
-export const getById = id => {
-    const filteredMovies = movies.filter( movie => id === movie.id);
-    return filteredMovies[0];
-}
+const LIST_URL = "https://yts.mx/api/v2/list_movies.json?";
+const DETAIL_URL = "https://yts.mx/api/v2/movie_details.json?";
+const SUGGESTION_URL = "https://yts.mx/api/v2/movie_suggestions.json?";
 
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter( movie => id !== movie.id);
-    if ( movies.length > cleanedMovies.length ) {
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = LIST_URL;
+    if ( limit > 0) {
+        REQUEST_URL += `limit=${limit}`;
+    } 
+    if ( rating > 0) {
+        REQUEST_URL += `&minimum_rating=${rating}`;
     }
-}
+    return fetch(`${REQUEST_URL}`)
+    .then(res => res.json())
+    .then(json => json.data.movies);
+};
 
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id : movies.length + 1,
-        name, 
-        score,
-    };
-    movies.push(newMovie);
-    return newMovie;
-}
+export const getMovie = (id) => {
+    let REQUEST_URL = DETAIL_URL;
+    REQUEST_URL += `movie_id=${id}`;
+    return fetch(`${REQUEST_URL}`)
+    .then(res => res.json())
+    .then(json => json.data.movie);
+};
+
+
+export const getSuggestions = (id) => {
+    let REQUEST_URL = SUGGESTION_URL;
+    REQUEST_URL += `movie_id=${id}`;
+    return fetch(`${REQUEST_URL}`)
+    .then(res => res.json())
+    .then(json => json.data.movies);
+};
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,10 @@
-import { movies, getById, deleteMovie, addMovie } from "../db/db"
+import { getMovies, getMovie, getSuggestions } from "../db/db"
 
 const resolvers = {
     Query : {
-        movies : () => movies, 
-        movie : (_, { id }) => getById(id)
-    }, 
-    Mutation : {
-        addMovie : (_, { name, score }) => addMovie(name, score),
-        deleteMovie : (_, { id }) => deleteMovie(id)
+        movies : (_, {limit, rating}) => getMovies(limit, rating),
+        movie: (_, { id }) => getMovie(id), 
+        suggestions : (_, { id }) => getSuggestions(id), 
     }
 };
 
